67f2ec1c23f76fd537fe37aa905727c4f8670166
mazerlodge <mazerlodge@hotmail.com>
2024-03-23 12:11:56 -0500

    Functional MVP.

--- doy.py
+++ doy.py
@@ -1,43 +1,60 @@
 
+# Global vars
+DAY_IDX = 0
+MONTH_IDX = 1
+YEAR_IDX = 2
+dateParts = []
+
+
+def getDaysInMonth(monthNum, yearNum):
+	# Return number of days in month provided.  
+	maxDaysInMonth = 0
+	
+	if (monthNum <= 7):
+		if (monthNum % 2 == 0):
+			maxDaysInMonth = 30
+		else:
+			maxDaysInMonth = 31
+	else: 
+		if (monthNum % 2 == 0):
+			maxDaysInMonth = 31
+		else:
+			maxDaysInMonth = 30
+
+	if (monthNum == 2):
+		maxDaysInMonth = 28
+
+		if (yearNum % 4 == 0):
+			maxDaysInMonth = 29
+			if (yearNum % 100 == 0) and (yearNum % 400 != 0):
+				maxDaysInMonth = 28
+					
+	return maxDaysInMonth
+	
+
 def isDateValid(rawDate):
 
 	bValid = False
-	dateParts = rawDate.split("/")
+	rawDateParts = rawDate.split("/")
 
 	try:
 		# validate parts are, you know, valid. 
-		monthPart = int(dateParts[0])
-		dayPart = int(dateParts[1])
-		yearPart = int(dateParts[2])
+		monthPart = int(rawDateParts[0])
+		dayPart = int(rawDateParts[1])
+		yearPart = int(rawDateParts[2])
 
 		# check range of values provided 
 		if monthPart > 12 or monthPart < 1 \
 		   or yearPart > 2250 or yearPart < 1776 \
 		   or dayPart > 31 or dayPart < 1:
 		   raise Exception("part of date out of range")
-	   
-		# do more precise dayPart checking
-		if (monthPart <= 7):
-			if (monthPart % 2 == 0):
-				maxDaysInMonth = 30
-			else:
-				maxDaysInMonth = 31
-		else: 
-			if (monthPart % 2 == 0):
-				maxDaysInMonth = 31
-			else:
-				maxDaysInMonth = 30
-	
-		if (monthPart == 2):
-			maxDaysInMonth = 28
-	
-			if (yearPart % 4 == 0):
-				maxDaysInMonth = 29
-				if (yearPart % 100 == 0) and (yearPart % 400 != 0):
-					maxDaysInMonth = 28
-				
-		# if we made it this far only 1 more check for validity
+		   
+		# make dur dayPart is within range of number of days in month.
+		maxDaysInMonth = getDaysInMonth(monthPart, yearPart)
 		if dayPart <= maxDaysInMonth:
+			dateParts.append(dayPart)
+			dateParts.append(monthPart)
+			dateParts.append(yearPart)
 			bValid = True
 		
 	except: 
@@ -45,12 +62,26 @@ def isDateValid(rawDate):
 
 	return bValid
 
-def countDaysToDate(rawDate):
-	print("countDaysToDate not yet implemented")
 
+def countDaysToDate(rawDate):
+	# Return number of days between Jan 1 and date specified, inclusive.
+	dayCount = 0 
+	
+	# add days for full months up to the month specified
+	for x in range(1,dateParts[MONTH_IDX]):
+		dayCount = dayCount + getDaysInMonth(x, dateParts[YEAR_IDX])
+
+	# add days for date specified up to (and including) day in date
+	dayCount = dayCount + dateParts[DAY_IDX]
+
+	return dayCount	
+
+
+# Main execution starts here.
 rawDate = input("Enter Date mm/dd/yyyy: ")
 if (isDateValid(rawDate)):
-	countDaysToDate(rawDate)
+	dayCount = countDaysToDate(rawDate)
+	print(f"Number of days (aka Julian Day) to {rawDate} is {dayCount}")
 else:
 	print("Unable to interpret date provided.")
 	
